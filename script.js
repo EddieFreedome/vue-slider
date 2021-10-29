e3daf171e4a64e70527689a1bbb0347e00967dc9
EddieFreedome <eddie.freed@tutanota.com>
2021-10-29 15:38:33 +0200

    Creato array oggetti

--- script.js
+++ script.js
@@ -27,7 +27,33 @@ window.addEventListener("DOMContentLoaded", function(){
     newVue ({
         el: "#root",
         data : {
-            
+            Image : [
+                {
+                    url: 'img/01.jpg',
+                    titolo: 'Svezia',
+                    descrizione: 'Svezia',
+                }
+                {
+                    url: 'img/01.jpg',
+                    titolo: 'Svizzera',
+                    descrizione: 'Svizzera',
+                }
+                {
+                    url: 'img/01.jpg',
+                    titolo: 'Gran Bretagna',
+                    descrizione: 'Gran Bretagna',
+                }
+                {
+                    url: 'img/01.jpg',
+                    titolo: 'Germania',
+                    descrizione: 'Germania',
+                }
+                {
+                    url: 'img/01.jpg',
+                    titolo: 'Paradise',
+                    descrizione: 'Paradise',
+                }
+            ]
         }
     })
 
