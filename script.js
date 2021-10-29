b7555b6722f25eeb74df04012d467233f46a20a1
EddieFreedome <eddie.freed@tutanota.com>
2021-10-29 16:02:43 +0200

    Inizializzato script, aggiornato html e css

--- script.js
+++ script.js
@@ -1,59 +1,39 @@
-const arrImg = [
-    'img/01.jpg',
-    'img/02.jpg',
-    'img/03.jpg',
-    'img/04.jpg',
-    'img/05.jpg'
-];
-
-const arrTitle = [
-    'Svezia',
-    'Svizzera',
-    'Gran Bretagna',
-    'Germania',
-    'Paradise'
-]
-
-const arrText = [
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    'Lorem ipsum',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-]
 
 window.addEventListener("DOMContentLoaded", function(){
-    VTTCue.config.devtools = true
-    newVue ({
+    Vue.config.devtools = true
+    new Vue ({
         el: "#root",
         data : {
-            Image : [
+            image : [
                 {
                     url: 'img/01.jpg',
                     titolo: 'Svezia',
                     descrizione: 'Svezia',
-                }
+                },
                 {
                     url: 'img/01.jpg',
                     titolo: 'Svizzera',
                     descrizione: 'Svizzera',
-                }
+                },
                 {
                     url: 'img/01.jpg',
                     titolo: 'Gran Bretagna',
                     descrizione: 'Gran Bretagna',
-                }
+                },
                 {
                     url: 'img/01.jpg',
                     titolo: 'Germania',
                     descrizione: 'Germania',
-                }
+                },
                 {
                     url: 'img/01.jpg',
                     titolo: 'Paradise',
                     descrizione: 'Paradise',
                 }
-            ]
+            ],
+
+
+
         }
     })
 
